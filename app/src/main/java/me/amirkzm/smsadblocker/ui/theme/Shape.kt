959b985aadac5615933b8126159a9007baecb029
amirk <amirkzm0098@gmail.com>
2022-10-28 23:11:14 +0330

    feat: migrated to dynamic material3

--- app/src/main/java/me/amirkzm/smsadblocker/ui/theme/Shape.kt
+++ app/src/main/java/me/amirkzm/smsadblocker/ui/theme/Shape.kt
@@ -1,11 +1,11 @@
 package me.amirkzm.smsadblocker.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(6.dp),
     medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    large = RoundedCornerShape(2.dp)
 )
--- app/src/main/java/me/amirkzm/smsadblocker/ui/theme/Type.kt
+++ app/src/main/java/me/amirkzm/smsadblocker/ui/theme/Type.kt
@@ -1,6 +1,6 @@
 package me.amirkzm.smsadblocker.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -13,23 +13,74 @@ val IranSans = FontFamily(
     Font(R.font.iransans_web_bold),
 )
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-    defaultFontFamily = IranSans,
-    body1 = TextStyle(
+    displaySmall = TextStyle(
+        fontFamily = IranSans,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp
     ),
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+    displayMedium = TextStyle(
+        fontFamily = IranSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 17.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = IranSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = IranSans,
+        fontWeight = FontWeight.Bold,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    headlineMedium = TextStyle(
+        fontFamily = IranSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = IranSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = IranSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 13.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = IranSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = IranSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = IranSans,
+        fontSize = 10.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = IranSans,
         fontSize = 12.sp
-    )
-    */
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = IranSans,
+        fontSize = 14.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = IranSans,
+        fontSize = 9.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = IranSans,
+        fontSize = 11.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = IranSans,
+        fontSize = 13.sp
+    ),
 )
